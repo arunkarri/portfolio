da2383f02221d33e252085df2cdaad4e324f1b8c
Arun Kashyap Karri <arun.kashyap@dltlabs.io>
2021-03-26 15:35:50 +0530

    reverting backend url

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,4 +1,4 @@
-const host = 'http://localhost:3000/';
-const resume= 'https://drive.google.com/file/d/1NhHiX-SWnTI2W05tXSyqsxj324oYyNY1/view?usp=sharing';
+const host = 'https://arunportfolio-backend.herokuapp.com/';
+const resume= 'https://drive.google.com/file/d/1vujhPSgzSA1U30B4nod1UQ2BbEudvbmN/view?usp=sharing';
 
 export default {host,resume};
